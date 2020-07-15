c8df57501445b093826a1ab9aa7198b154a86dfe
Fabian Quoidbach <fabianquoidbach@gmail.com>
2020-07-15 14:59:30 +0200

    Drill conditions terminé

--- conditions/conditions.php
+++ conditions/conditions.php
@@ -4,14 +4,14 @@
 
 <?php
 $possible_states = array("health hazard", "filthy", "dirty", "clean", "immaculate");
-$room_filthiness = $possible_states[4];
+$room_filthiness = $possible_states[0];
 
 
-if( $room_filthiness == "health hazard" ){
-    echo "Yuk, Room is dirty : let's clean it up !";
-} else if ($room_filthiness == "filthy" ){
-    echo "Yuk, Room is dirty : let's clean it up !";
-} else if ($room_filthiness == "dirty"){
+if( $room_filthiness == $possible_states[0] ){
+    echo "Yuk, Room is hazardous : let's clean it up !";
+} else if ($room_filthiness == $possible_states[1]){
+    echo "Yuk, Room is filthy : let's clean it up !";
+} else if ($room_filthiness == $possible_states[2]){
     echo "Yuk, Room is dirty : let's clean it up !";
 } else {
     echo "Nothing to do, your room is neat !";
