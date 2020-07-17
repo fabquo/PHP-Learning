c4fd3631542ea0217e0cbe158911737f5fbb2089
Fabian Quoidbach <fabianquoidbach@gmail.com>
2020-07-17 16:38:26 +0200

    Start OOP

--- OOP/recipes.php
+++ OOP/recipes.php
@@ -17,6 +17,15 @@ class Recipe{
     public $tag = array();
     public $source = 'The Crazy cooker';
 
+    private $measurements = array(
+        'liter',
+        'g',
+        'cup',
+        'kg',
+        'c. à soupe',
+        'c. à café'
+    );
+
     public function displayRecipe(){
         return $this->title . ' by ' . $this->source;
     }
@@ -28,16 +37,43 @@ class Recipe{
     public function getTitle(){
         return $this->title;
     }
+
+    public function addIngredients($item, $amount = null, $measure = null){
+
+        if($amount != null && !is_float($amount) && !is_int($amount)){
+            exit('The amount must be a float: ' . gettype($amount) . ' $amount given.');
+        }
+
+        if($measure != null && !in_array(strtolower($measure),$this->measurements)){
+            exit('please enter a valid measurement :' . implode(', ', $this->measurements));
+        }
+
+        $this->ingredients[] = array(
+            'item'      =>  ucwords($item),
+            'amount'    =>  $amount,
+            'measure'   =>  $measure
+        );
+    }
+
+    public function getIngredients(){
+        return $this->ingredients;
+    }
+
 }
 
 $recipe1 = new Recipe();
 $recipe1->setTitle("my first recipe");
+$recipe1->addIngredients('Sel', 2, 'c. à café');
 
 echo '<br>' . $recipe1->getTitle();
 
 echo '<br>' . $recipe1->source;
 
 echo '<br>' . $recipe1->displayRecipe();
+
+foreach ($recipe1->getIngredients() as $ing){
+    echo '<br>' . $ing['amount'] . ' ' . $ing['measure'] . ' of ' . $ing['item'] . '<br>';
+}
 
 ?>
 
