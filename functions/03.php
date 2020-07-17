d03b5586296ad4a2b38a90ece8e9c8c33f863c3d
Fabian Quoidbach <fabianquoidbach@gmail.com>
2020-07-17 12:33:32 +0200

    Random words ok

--- functions/03.php
+++ functions/03.php
@@ -16,9 +16,18 @@
         function generateWords(){
             
             $message = '';
+            $randLgt1 = rand(1,5);
+            $randLgt2 = rand(7,15);
             
             if(isset($_POST['generate'])){
-                $message = 'test';
+                
+                $word = range('a','z');
+                $word2 = range('a','z');
+                shuffle($word);
+                shuffle($word2);
+                $string = implode($word);
+                $string2 = implode($word2);
+                $message = substr($string,0, $randLgt1) . ' ' . substr($string2,0,$randLgt2);
             }
             
             return $message;
@@ -27,7 +36,7 @@
 
     <h1>Generate a new word</h1>
 
-    <?php echo generateWords(); ?>
+    <?php echo '<p>' . generateWords() . '</p>'; ?>
     <form method="POST">
         <button name="generate" value="true">Generate</button>
     </form>
